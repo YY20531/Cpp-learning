da1bdbfd66e6226c352a243bb0a70703148ab725
Yuan Yi <3066671772@qq.com>
2026-03-29 17:05:01 +0800

    Update Practice.cpp

--- 2026/03-29/Practice.cpp
+++ 2026/03-29/Practice.cpp
@@ -1,9 +1,35 @@
-#include <iostream>
+#include <iostream> 
 
-using namespace std;
-
-int main()
+using namespace std; 
+ 
+class Point
 {
-  
-  return 0;
+private:
+    int x, y;
+public:
+    Point(int a, int b): x(a), y(b){};
+    
+    friend ostream& operator<<(ostream &os, const Point &a);
+    friend Point operator+(Point &a, Point &other);
+    int get_x(){return x;}
+    int get_y(){return y;}
+};
+
+ostream& operator<<(ostream &os, const Point &a)
+{
+    os << "(" << a.x << ", " << a.y << ")";
+    return os;
+}
+
+Point operator+(Point & a, Point &other)
+{
+    return Point(a.x + other.get_x(), a.y + other.get_y());
+}
+int main() 
+{ 
+    Point a(1, 2);
+    Point b(3, 4);
+    
+    cout << a + b << endl;
+    return 0;
 }
